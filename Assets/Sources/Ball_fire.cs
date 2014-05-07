6f4649db7220587c63a1561a7859b6b0473d51e9
liberty1111 <lhs3110@naver.com>
2014-05-07 17:17:08 +0900

    발사체 추가 & 충돌시 이펙트 추가

---
+++ Assets/Sources/Ball_fire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball_fire : MonoBehaviour {
+	public Transform ball;
+	Transform spPoint;
+	public int power=1000;
+	// Use this for initialization
+	void Start () {
+		spPoint = GameObject.Find ("spPoint").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+		   FireBullet();
+	}
+    void FireBullet()
+    {
+			Transform obj = Instantiate (ball, spPoint.position, Quaternion.identity) as Transform; 
+		obj.rigidbody.AddForce (-1 * power, 0.3f*power, 0);
+
+		
+	}
+}
---
+++ Assets/Sources/CollisionEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionEvent : MonoBehaviour {
+	public int power=1000;
+	public Transform explode;
+	// Use this for initialization
+
+	void OnCollisionEnter (Collision Collsion) {
+		//this.gameObject.rigidbody.AddForce (0,1 * power,1 * power);
+		Object obj = Instantiate (explode, transform.position, Quaternion.identity);
+		//Destroy (gameObject);
+		Destroy (obj, 2);
+	}
+}
