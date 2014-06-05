ff8bb073e67feca7d77f92b1c0a2f0923c6061ce
liberty1111 <lhs3110@naver.com>
2014-06-05 12:37:13 +0900

    발사체 추가&발사체 종류 랜덤하게 발사

--- Assets/Sources/ProjectileThrower.cs
+++ Assets/Sources/ProjectileThrower.cs
@@ -5,12 +5,13 @@ public class ProjectileThrower : MonoBehaviour
 {
 	private const string THROWING_POINT_NAME = "Throwing_Point";
 
-	public Transform projectile;
+	public Transform projectile1,projectile2,projectile3,projectile4,projectile5;
 
 	public int powerX;
 	public int powerY;
 	public int powerZ;
 
+	public int matKind;
 	public float throwRate;
 
 	private Transform startPoint;
@@ -26,7 +27,7 @@ public class ProjectileThrower : MonoBehaviour
 	{
 		powerZ = Random.Range (2400, 3000);
 		powerX = Random.Range (-700, 700);
-
+		matKind = (int)Random.Range (1, 6);
 		if (Random.Range (0, 1000) < (int)(throwRate * 100.0f))
 			FireBullet ();
 	}
@@ -34,7 +35,33 @@ public class ProjectileThrower : MonoBehaviour
 
     void FireBullet()
     {
-		Transform obj = Instantiate (projectile, startPoint.position, Quaternion.identity) as Transform; 
-		obj.rigidbody.AddForce (powerX, 0.3f * powerY, powerZ * -1);
+		if (matKind == 1) 
+		{
+			Transform obj = Instantiate (projectile1, startPoint.position, Quaternion.identity) as Transform; 
+			obj.rigidbody.AddForce (powerX, 0.3f * powerY, powerZ * -1);
+		}
+		else if (matKind == 2) 
+		{
+			Transform obj = Instantiate (projectile2, startPoint.position, Quaternion.identity) as Transform; 
+			obj.rigidbody.AddForce (powerX, 0.3f * powerY, powerZ * -1);
+		}
+		
+		else if (matKind == 3) 
+		{
+			Transform obj = Instantiate (projectile3, startPoint.position, Quaternion.identity) as Transform; 
+			obj.rigidbody.AddForce (powerX, 0.3f * powerY, powerZ * -1);
+		}
+		
+		else if (matKind == 4) 
+		{
+			Transform obj = Instantiate (projectile4, startPoint.position, Quaternion.identity) as Transform; 
+			obj.rigidbody.AddForce (powerX, 0.3f * powerY, powerZ * -1);
+		}
+		else if (matKind == 5) 
+		{
+			Transform obj = Instantiate (projectile5, startPoint.position, Quaternion.identity) as Transform; 
+			obj.rigidbody.AddForce (powerX, 0.3f * powerY, powerZ * -1);
+		}
+
 	}
 }
